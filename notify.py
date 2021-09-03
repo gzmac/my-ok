45c6397bcdda3902a8eb1bf905c3a7a3b0ab22ee
gzmac <gdmac@qq.com>
2021-09-03 11:45:56 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -64,7 +64,7 @@ if QYWX_APP:
     notify_mode.append('qywxapp_bot')
     print("企业微信应用 推送打开")
 if PUSH_PLUS_TOKEN:
-    notify_mode.append('push_plus_notif')
+    notify_mode.append('push_plus_notify')
     print("PUSH+ 推送打开")
     
     
